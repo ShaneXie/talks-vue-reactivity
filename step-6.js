ab45e8cc5a8107c2ee550c27cd123072c03a85c6
Axe <axe@axesk.com>
2019-04-21 21:24:27 -0500

    proxy

--- step-6.js
+++ step-6.js
@@ -1,1 +1,30 @@
 const withProxy = new Proxy(target, handler);
+
+const depsStorage = new WeakMap();
+const handlers = {
+  get(target, key, receiver) {
+    let deps = depsStorage.get(target);
+    if (!deps) {
+      deps = {};
+      depsStorage.set(target, deps);
+    }
+    let dep = deps[key];
+    if (!dep) {
+      dep = deps[key] = new Dep();
+    }
+    dep.depend();
+    return observable(target[key]);
+  },
+  set(target, key, value) {
+    target[key] = value;
+    // notify
+    let deps = depsStorage.get(target);
+    if (!deps) {
+      return;
+    }
+    const dep = deps[key];
+    if (dep) {
+      dep.notify();
+    }
+  }
+};
